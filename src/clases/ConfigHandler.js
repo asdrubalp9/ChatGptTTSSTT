1bf0c6ef2171d9f8b13574ff97fdbf8d76955360
Asdrúbal Pérez <asdrubalp9@gmail.com>
2023-06-07 08:20:24 -0400

    partially working config handler

---
+++ src/clases/ConfigHandler.js
@@ -0,0 +1,48 @@
+import { config } from './../config.js';
+
+export default class ConfigHandler {
+  constructor() {
+    this.settings = {};
+    
+    chrome.storage.onChanged.addListener((changes, areaName) => {
+        console.log('changes', changes, 'areaName', areaName)
+      if (areaName === 'sync') {
+        this.updateSettings(changes);
+      }
+    });
+    
+    this.getSettings();
+  }
+
+    static async create() {
+        const handler = new ConfigHandler();
+        await handler.initialize();
+        return handler;
+    }
+
+  getSettings() {
+    return new Promise((resolve, reject) => {
+      
+      const configKeys = config.filter((field) => field?.name !== undefined);
+      const configKeyNames = configKeys.map(key => key.name);
+      chrome.storage.sync.get(configKeyNames, (result) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          for (const key of configKeys) {
+            this.settings[key.name] = result[key.name] || key.defaultValue;
+          }
+          console.log("Settings loaded:", this.settings);
+          resolve();
+        }
+      });
+    });
+  }
+
+  updateSettings(changes) {
+    for (let key in changes) {
+      let storageChange = changes[key];
+      this.settings[key] = storageChange.newValue;
+    }
+  }
+}
