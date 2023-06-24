4073d2e2c9a5f6e83d4ea16beb89a8b3ac7335ad
Asdrúbal Pérez <asdrubalp9@gmail.com>
2023-06-24 11:02:45 -0400

    pup

--- src/clases/TextoAVoz.js
+++ src/clases/TextoAVoz.js
@@ -6,10 +6,10 @@ export default class TextoAVoz {
     this.utterance = new SpeechSynthesisUtterance();
     this.settings = {};
     this.state = 'playing';
-    this.utterance.lang = "en-US";
+    this.utterance.lang = "en-US"; //agarrar el default del navegador
     this.utterance.lang = "es-ES";
     this.initialized = this.init();
-
+    this.addScreenToCancel()
     chrome.storage.onChanged.addListener((changes, areaName) => {
       if (areaName === 'sync') {
         this.updateSettings(changes);
@@ -90,24 +90,37 @@ export default class TextoAVoz {
   }
   
   addScreenToCancel() {
+    const readingIn = ''
     const htmlContent = `
         <style>
           .flex-center {
             justify-content: center;
             align-items: center;
             display: flex;
-        }
+          }
+          .column {
+            flex-direction: column;
+          }
         </style>
-        <div class="flex flex-col justify-center">
-          <button class="btn flex-center btn-outline-dark flex-grow border border-gray-200" id="speechState" data-state="playing">
-            <svg style="width: 100px;" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
-          </button>
-          <button class="btn flex-center btn-danger flex-grow" id="cancelSpeech" style="margin:20px 0px;">
-            <svg style="width: 100px;" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>
-          </button>
-          <p style="text-align:center">
-            ${geti18nMessage('PressEscToCancel')}
-          </p>
+        <div class="flex flex-col justify-center row">
+          <div class="col-12">
+            <p>
+            ${geti18nMessage('readingIn')}: 
+            </p>
+          </div>
+          <div class="col-6">
+            <button class="btn flex-center btn-outline-dark flex-grow border border-gray-200" id="speechState" data-state="playing">
+              <svg style="width: 100px;" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
+            </button>
+          </div>
+          <div class="col-6">
+            <button class="btn flex-center btn-danger flex-grow" id="cancelSpeech" style="margin:20px 0px 0px;">
+              <svg style="width: 100px;" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>
+            </button>
+            <p style="text-align:center">
+              ${geti18nMessage('PressEscToCancel')}
+            </p>
+          </div>
         </div>
       `;
     const div = this.textToSpeechWindow(htmlContent);
